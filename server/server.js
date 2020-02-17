d7fa43c27aaa35b4363b75f7855c735bdd374207
george spanos <gspanos@inttrust.gr>
2020-02-17 20:28:33 +0200

    view engine

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const publicweb = process.env.PUBLICWEB || './';
 const devPath = 'dist'
 const app = express();
-
+app.set('view engine', 'html');
 app.use(express.static(publicweb));
 console.log(`serving ${publicweb}`);
 app.get('*', (req, res) => {
