0d0bf9f84dccfd864537eb962a3b6418335b76c0
george spanos <gspanos@inttrust.gr>
2020-02-17 20:30:02 +0200

    body parser install
    fix
    
    parser fix
    
    tet1
    
    test111
    
    port fix
    
    testt1111

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 const publicweb = process.env.PUBLICWEB || './';
-const devPath = 'dist'
-const app = express();
-app.set('view engine', 'html');
+const devPath = 'dist';
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(publicweb));
+
+app.set('view engine', 'html');
 console.log(`serving ${publicweb}`);
 app.get('*', (req, res) => {
   res.sendFile(`index.html`, { root: publicweb });
 });
 
-const port = process.env.SERVER_PORT || '3000';
+const port = process.env.PORT || '3000';
 app.listen(port, () => console.log(`API running on localhost:${port}`));
